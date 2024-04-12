4376320fec2d61531cc7f9c2e2031ec6487d8e79
oscarIM <oskinostroza@gmail.com>
2024-04-12 13:46:26 -0400

    testin x-xis

--- R/sources_fn_abiotic.R
+++ R/sources_fn_abiotic.R
@@ -194,8 +194,8 @@ fn_plot_bar_abiotic <- function(data, col_pars, col_sitio, col_valor, col_grupo 
         strip.text = element_text(size = 6, family = "Arial"),
         axis.text.x = element_text(angle = angle, 
                                        hjust = 1, 
-                                       vjust = 0.5) +
-        theme_bw()
+                                       vjust = 0.5) #+
+        #theme_bw()
       )
     ggsave(filename = paste0("bar_pars_", ord_sitio, "_", matriz, ".png"), plot = plot, width = width, height = height, dpi = 300)
   }
